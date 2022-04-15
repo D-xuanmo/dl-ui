d6acc388a745eb3cf182862cff864cf19892aa32
D-xuanmo <me@xuanmo.xin>
2022-04-15 22:58:15 +0800

    feat: initialize project

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:vue/vue3-recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    semi: 0,
+    indent: ['warn', 2]
+  }
+}
