3d34fe4bbd919087765c558bc22c8b4f36f215ec
D-xuanmo <me@xuanmo.xin>
2022-05-30 23:17:00 +0800

    refactor: TS 声明更新

--- packages/types.d.ts
+++ packages/types.d.ts
@@ -5,3 +5,15 @@ declare module 'vue' {
     className?: string
   }
 }
+
+declare module '*.vue' {
+  import type { ComponentOptions } from 'vue'
+  const Component: ComponentOptions
+  export default Component
+}
+
+declare module '*.md' {
+  import type { ComponentOptions } from 'vue'
+  const Component: ComponentOptions
+  export default Component
+}
