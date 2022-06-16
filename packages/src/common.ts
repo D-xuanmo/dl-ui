4ca94dedc06bd39f7b6e0bd5271dec9717b7779b
D-xuanmo <me@xuanmo.xin>
2022-06-16 11:07:23 +0800

    refactor: 公用类型修改

--- packages/src/common.ts
+++ packages/src/common.ts
@@ -9,8 +9,11 @@ export type FieldFormatterTrigger = 'onChange' | 'onBlur'
 /** 弹框位置类型 */
 export type PlacementType = 'top' | 'right' | 'bottom' | 'left' | 'center'
 
-/** 数据类型，单选、多选、选择器等组件 */
+/** 数据基础类型，单选、多选、选择器等组件 */
 export type DataType = {
   label: string
   value: string | number
 } & Record<string, unknown>
+
+/** 排除 props value / modelValue */
+export type OmitValueProperties<T extends Record<string, unknown>> = Omit<T, 'value' | 'modelValue'>
