2413975b84c468c877deab3f790f1a629b158a94
D-xuanmo <me@xuanmo.xin>
2023-05-07 12:09:36 +0800

    fix(Cell): 修复单元格 inject 警告问题

--- packages/src/cell/utils.ts
+++ packages/src/cell/utils.ts
@@ -3,8 +3,8 @@ import { inject } from 'vue'
 import { CELL_GROUP_CONTEXT_KEY, GLOBAL_CONFIG_CONTEXT_KEY } from '../context'
 
 export const useGlobalConfig = (props: CellProps) => {
-  const globalConfig = inject(GLOBAL_CONFIG_CONTEXT_KEY) ?? {}
-  const { cellTitleWidth, cellContentAlign, layout, border } = inject(CELL_GROUP_CONTEXT_KEY) ?? {}
+  const globalConfig = inject(GLOBAL_CONFIG_CONTEXT_KEY, {})
+  const { cellTitleWidth, cellContentAlign, layout, border } = inject(CELL_GROUP_CONTEXT_KEY, {})
   return {
     labelWidth: props.titleWidth || cellTitleWidth || globalConfig.labelWidth,
     contentAlign: props.contentAlign || cellContentAlign || globalConfig.contentAlign || 'left',
