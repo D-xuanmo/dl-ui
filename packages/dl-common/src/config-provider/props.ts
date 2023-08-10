d9247a08fab638d3a3e6e4c5f77127e6fd984aac
D-xuanmo <me@xuanmo.xin>
2023-08-10 22:00:21 +0800

    fix: 修复 form 丢失 TS 类型问题

--- packages/dl-common/src/config-provider/props.ts
+++ packages/dl-common/src/config-provider/props.ts
@@ -5,5 +5,5 @@ import { FORM_PROPS } from '../form'
 export type ConfigProviderProps = ExtractPropTypes<typeof CONFIG_PROVIDER_PROPS>
 
 export const CONFIG_PROVIDER_PROPS = {
-  ...pickProps(FORM_PROPS, ['requiredMarkPosition', 'layout', 'labelWidth'])
+  ...pickProps(FORM_PROPS, ['colon', 'requiredMarkPosition', 'layout', 'labelWidth'])
 }
