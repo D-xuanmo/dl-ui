a406ea7d6bce0f15ffd130efaaebc1132dd8505c
D-xuanmo <me@xuanmo.xin>
2024-05-21 23:03:25 +0800

    build: 文档打包配置调整

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -8,6 +8,9 @@ import MarkdownPreview, { transformer } from '@xuanmo/vite-plugin-md-preview'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: true
+  },
   plugins: [
     vue({
       include: [/\.vue$/, /\.md$/]
