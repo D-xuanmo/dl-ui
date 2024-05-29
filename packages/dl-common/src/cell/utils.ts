cc806baf4fbf869fdcf9427c48b3cbedce8a8c4d
D-xuanmo <me@xuanmo.xin>
2024-05-29 23:50:53 +0800

    perf(Cell): 优化单元格样式

--- packages/dl-common/src/cell/utils.ts
+++ packages/dl-common/src/cell/utils.ts
@@ -2,6 +2,7 @@ import { CellProps } from './props'
 import { computed, inject } from 'vue'
 import { CELL_GROUP_CONTEXT_KEY } from '../cell-group/context'
 import { useConfig } from '../hooks'
+import { ClientTypeEnum } from '../constants'
 
 export const useGlobalConfig = (props: CellProps) => {
   const { cellTitleWidth, cellContentAlign, layout, border, round } = inject(
@@ -13,11 +14,12 @@ export const useGlobalConfig = (props: CellProps) => {
     props as any
   )
   return computed(() => {
+    const clientType = props.clientType || config.value.clientType
     return {
       clientType: props.clientType || config.value.clientType,
       hideTitle: props.hideTitle,
       border: props.border ?? border ?? config.value.border,
-      round: props.round ?? round ?? config.value.round,
+      round: clientType === ClientTypeEnum.PC ? false : props.round ?? round ?? config.value.round,
       contentAlign: props.contentAlign || cellContentAlign || 'left',
       labelWidth: props.titleWidth || cellTitleWidth || config.value.labelWidth,
       layout: props.layout || layout?.value || config.value.layout || 'horizontal',
